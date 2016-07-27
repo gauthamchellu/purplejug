39847495f6cde747486ccebb34798e82cf6a9616
Gautham Chelladurai <gauthamchellu@gmail.com>
2016-07-27 22:14:17 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+	<title>Purple Jug - Tradition and Toys</title>
+	<link rel="stylesheet" type="text/css" href="styles/purplejug-styles.css">
+</head>
+<body>
+	<div id="purplejug-logocontainer" class="purplejug-logocontainer-top">
+		<h1 id="purplejug-mainlogo">Purple Jug</h1>
+	</div>
+</body>
+</html>
